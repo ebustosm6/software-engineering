b0587879c4631a7772dcb237168e54fb641eca88
ebustosm6 <eduardobustosmiranda@hotmail.com>
2020-03-24 18:30:15 +0100

    initial commit

---
+++ Assignment2/assignment/src/main/java/softwareeng/App.java
@@ -0,0 +1,13 @@
+package softwareeng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
